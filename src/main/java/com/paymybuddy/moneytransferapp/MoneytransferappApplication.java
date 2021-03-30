16e8eacbd676b6a315b6e2ac9ec6bbbe8ae49a81
xhxcx <xxhxcxx.sn@gmail.com>
2021-03-30 16:10:48 +0200

    init spring project

---
+++ src/main/java/com/paymybuddy/moneytransferapp/MoneytransferappApplication.java
@@ -0,0 +1,13 @@
+package com.paymybuddy.moneytransferapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneytransferappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneytransferappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/paymybuddy/moneytransferapp/MoneytransferappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paymybuddy.moneytransferapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneytransferappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
